7892661d5708a140992d9d7d570de69a22977206
PedrinhodaGaiola02 <pedrobrunomonteiromonteiro@gmail.com>
2024-08-29 18:05:15 -0300

    Update main.py

--- ex02/main.py
+++ ex02/main.py
@@ -3,41 +3,52 @@ import numpy as np
 import time 
 import matplotlib.pyplot as plt
 
-dimens = [50, 100, 150, 200]
-#dimr = [50, 100, 150, 200]
-#dimn = [50, 100, 150, 200]
+dimenm = [100, 150, 200, 250, 300]
+dimenr = [100, 150, 200, 250, 300]
+dimenn = [100, 150, 200, 250, 300]
 tempos1 = []
 tempos2 = []
-for i in range (len(dimens)):
-    n = dimens[i]
-    A = np.random.rand(n,n)
-    B = np.random.rand(n,n)
+for i in range (len(dimenm)):
+    m = dimenm[i]
+    for j in range(len(dimenr)):
+        r = dimenr[j]
+        for k in range(len(dimenn)):
+            n = dimenn[k]
+            A = np.random.rand(m,r)
+            B = np.random.rand(r,n)
+            
 
-    def multiplica_lenta (A, B, n):
-        C1 = np.zeros(shape =(n,n), dtype=np.float64)
-        for i in range(n):
-            C1[i,i] += A[i,i]*B[i,i]
-        return C1
+            def multiplica_lenta (A, B, m, r, n):
+                C1 = np.zeros(shape =(m,n), dtype=np.float64)
+                for i in range(m):
+                    for j in range(r):
+                        for k in range(n):
+                            C1[i,k] += A[i,j]*B[j,k]
+                return C1
 
-    tinicial = time.time()
-    C1 = multiplica_lenta(A, B, n)
-    tempo_corrido1 = time.time() - tinicial
+            tinicial = time.time()
+            C1 = multiplica_lenta (A, B, m, r, n)
+            tempo_corrido1 = time.time() - tinicial
+            
+            def multiplica_rapida (A, B, m, r, n):
+                C2 = A@B
+                return C2
 
-    #print('Tempo corrido para calcular1 =', tempo_corrido1, '\n')
+            tinicial = time.time()
+            C2 = multiplica_rapida(A, B, m, r, n)
+            tempo_corrido2 = time.time() - tinicial
 
-    def multiplica_rapida (A, B, n):
-        C2 = A@B
-        return C2
-
-    tinicial = time.time()
-    C2 = multiplica_rapida(A, B, n)
-    tempo_corrido2 = time.time() - tinicial
-
-    #print('Tempo corrido para calcular2 =', tempo_corrido2, '\n')
-    tempos1.append(tempo_corrido1)
-    print('Tabela de tempos da multilenta e', i, n, tempos1[i], '\n')
-    tempos2.append(tempo_corrido2)
-    print('Tabela de tempos da multirapida e', i, n, tempos2[i], '\n')
+            tempos1.append(tempo_corrido1)
+            tempos2.append(tempo_corrido2)
 
 
+
+plt.loglog(dimenm, tempos1, '-r')
+plt.loglog(dimenm, tempos2, '-g')
+plt.xlabel('dimensao')
+plt.ylabel('tempos [s]')
+plt.title('Grafico dimensao X tempo')
+plt.legend(['multiplica_lenta', 'multiplica_rapida'])
+plt.grid()
+plt.show()
 
