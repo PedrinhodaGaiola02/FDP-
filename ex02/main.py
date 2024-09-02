0b26588e1aad33c79d4fd42b560bdb038bf5a52b
PedrinhodaGaiola02 <pedrobrunomonteiromonteiro@gmail.com>
2024-09-02 17:42:53 -0300

    Deu certo o Exercício 2

--- ex02/main.py
+++ ex02/main.py
@@ -2,12 +2,10 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from lenta import multiplica
+from lenta import multiplica as multiplica_lenta
+from rapida import multiplica as multiplica_rapida
 
-dimensoes = [100, 150, 200, 250, 300]
-tempos = []
-
-for dimensao in dimensoes:
+def mede_tempo(multiplica):
     tempo_inicial = time.time()
 
     matrizA = np.random.rand(dimensao, dimensao)
@@ -18,12 +16,25 @@ for dimensao in dimensoes:
 
     tempo_final = time.time()
     tempo_percorrido = tempo_final - tempo_inicial
-    tempos.append(tempo_percorrido)
 
-plt.loglog(dimensoes, tempos, '-r')
+    return tempo_percorrido
+
+dimensoes = [100, 250, 500, 700, 1000]
+tempos_lenta = []
+tempos_rapida = []
+
+for dimensao in dimensoes:
+    tempo_lenta_percorrido = mede_tempo(multiplica_lenta)
+    tempo_rapida_percorrido = mede_tempo(multiplica_rapida)
+    tempos_lenta.append(tempo_lenta_percorrido)
+    tempos_rapida.append(tempo_rapida_percorrido)
+
+plt.loglog(dimensoes, tempos_lenta, '-r')
+plt.loglog(dimensoes, tempos_rapida, '-g')
 plt.xlabel('dimensao')
 plt.ylabel('tempos [s]')
 plt.title('Grafico dimensao X tempo')
+plt.legend(['Lenta', 'Rapida'])
 plt.grid()
 plt.show()
 
