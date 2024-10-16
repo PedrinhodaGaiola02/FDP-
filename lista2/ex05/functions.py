afa2a27a57c5b7787613ca9fb8aef67c5ebac641
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2024-10-16 16:40:34 -0300

    desenha parabolas

---
+++ lista2/ex05/functions.py
@@ -0,0 +1,14 @@
+from math import pi, sin, cos
+
+def f(x):
+    return 1 + 0.5 * sin(2*x) ** 3
+
+def g(x):
+    return 3 + 0.5 * cos(3*x) ** 5
+
+def h(y):
+    return (1/1.5625) * (y**2 - 4.5*y + 3.5)
+
+def j(y):
+    return (-1/1.5625) * (y**2 - 4.5*y + 3.5) + 2*pi
+
---
+++ lista2/ex05/main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import pi
+
+from functions import f, g, h, j
+from tools import image, monte_carlo
+from graphs import plot, scatter
+
+dominio = np.linspace(0, 2*pi, 100)
+
+imagem_f = image(f, dominio)
+imagem_g = image(g, dominio)
+
+dominio2 = np.linspace(f(0), g(0))
+
+imagem_h = image(h, dominio2)
+imagem_j = image(j, dominio2)
+
+plot(dominio, imagem_f, imagem_g, dominio2, imagem_h, imagem_j)
+
+# area, pontos_dentro, pontos_fora = monte_carlo(f, g, dominio, number_of_points=100000)
+# scatter(pontos_dentro, pontos_fora)
+
+plt.show()
