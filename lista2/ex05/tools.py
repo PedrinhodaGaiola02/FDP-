099c2f706b4d5cc3b79866424ed09a021c48aacc
PedrinhodaGaiola02 <pedrobrunomonteiromonteiro@gmail.com>
2024-10-17 17:28:10 -0300

    ex05 com os pontos sendo gerados do -2 para o 8 no eixo x, pq do -1 tava muito ruim :)

--- lista2/ex05/tools.py
+++ lista2/ex05/tools.py
@@ -5,13 +5,13 @@ from progress.bar import Bar
 def image(function, domain):
     return tuple(map(function, domain))
 
-def monte_carlo(f, g, h, j, number_of_points, label="Estimando área via método de Monte Carlo..."):
-    points = np.random.uniform((-1, 0), (8, 4), (number_of_points, 2))
+def monte_carlo(f, g, h, j, number_of_points):
+    points = np.random.uniform((-2, 0), (8, 4), (number_of_points, 2))
 
     inside = []
     outside = []
 
-    bar = Bar(label, max=number_of_points)
+    bar = Bar("Estimando área via método de Monte Carlo...", max=number_of_points)
     for (x, y) in points:
         if ((f(x) < y and g(x) > y) or (x < 0 or x > 2*pi)) and ((h(y) < x and j(y) > x) or ((y < 1 or y > g(x)) and (x > 0 and x < 2*pi))):
             inside.append((x, y))
@@ -25,3 +25,4 @@ def monte_carlo(f, g, h, j, number_of_points, label="Estimando área via método
     figure_area = (len(inside) / number_of_points) * retangle_area
 
     return figure_area, np.array(inside), np.array(outside)
+
