a9344ec0bc29d0718478894cc74da18a78ce5cf1
PedrinhodaGaiola02 <pedrobrunomonteiromonteiro@gmail.com>
2024-10-22 17:37:03 -0300

    Ex07 em 3D baby

--- lista2/ex07/main.py
+++ lista2/ex07/main.py
@@ -2,12 +2,15 @@ from matplotlib import pyplot as plt
 
 from domain import r
 from graphic import plot
+from mpl_toolkits.mplot3d import Axes3D
 
 viagem = r(10000)
 plot(viagem)
 
-plt.plot(-50, 50)
-plt.plot(50, -50)
-plt.plot(50, 50)
-plt.plot(-50, -50)
+plt.plot(-50, 50, 50)
+plt.plot(50, -50, 50)
+plt.plot(50, 50, -50)
+plt.plot(-50, -50, 50)
+plt.plot(-50, -50, -50)
+plt.plot(50, 50, 50)
 plt.show()
