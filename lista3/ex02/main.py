6fe2ed736dba23774a151f0c3a2ebe18d289206b
PedrinhodaGaiola02 <pedrobrunomonteiromonteiro@gmail.com>
2024-11-21 15:27:47 -0300

    mudanças

--- lista3/ex02/main.py
+++ lista3/ex02/main.py
@@ -1,33 +1,30 @@
-from math import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def phi(x):
-    phi = 1/sqrt(x)
+    phi = 1/np.sqrt(x)
     return phi 
 
 x_velho = 0.75
 
 
-iteracoes = 100
-valores_de_x = [0.75, ]
-valores_de_phi = [1.1547005383792517, ]
+iteracoes = 2
+valores_de_x = [x_velho]
+valores_da_funcao = []
 
 for i in range(iteracoes):
     x_novo = phi(x_velho)
-    valores_de_phi.append(phi(x_velho))
-    x_velho = x_novo
+    valores_da_funcao.append(phi(x_velho))
     valores_de_x.append(x_novo)
-    #print('Valor de X:', valores_de_x)
-
+    x_velho = x_novo
+    print('Valor de X:', valores_de_x)
+    print('Valor de da funcao:', valores_da_funcao)
 
 def y(x):
-    y = x
-    return y
+    return x
 
+eixo_x = np.linspace(0, 1.5)
 
-m = np.linspace(0, 1.5)
-print(m)
-
-
+plt.plot(eixo_x, phi(eixo_x))
+plt.plot(eixo_x, y(eixo_x), linestyle='--')
+plt.show()
