14d89e478e798e99ff025a7e8708c72a1d7286b0
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2024-11-26 16:53:41 -0300

    ex01 feito

---
+++ lista4/ex01/main.py
@@ -0,0 +1,20 @@
+from matplotlib import image
+from matplotlib import pyplot as plt
+
+img = image.imread('stinkbug.png')
+
+print(img.shape)
+
+A = img[:, :, 0]
+
+plt.imshow(img)
+plt.show()
+
+plt.imshow(A)
+plt.show()
+
+plt.imshow(A, cmap='hot')
+plt.show()
+
+plt.imshow(A, cmap='nipy_spectral')
+plt.show()
