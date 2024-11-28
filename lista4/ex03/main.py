db911ff7062dda32ce63216b8b1041bfb79c6784
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2024-11-28 15:17:34 -0300

    suavizacao ok

--- lista4/ex03/main.py
+++ lista4/ex03/main.py
@@ -1,11 +1,55 @@
 from matplotlib import image
 from matplotlib import pyplot as plt
+from progress.bar import Bar
 
 img = image.imread('stinkbug.png')
 print("Shape:", img.shape)
 
-def suaviza(img):
-    return img
 
-plt.imshow(suaviza(img))
+def suaviza_retangulo(img):
+    new_img = img.copy()
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+            media = (
+                img[i - 1, j - 1] + img[i - 1, j] + img[i - 1, j + 1] +
+                img[i,     j - 1] + img[i,     j] + img[i,     j + 1] +
+                img[i + 1, j - 1] + img[i + 1, j] + img[i + 1, j + 1]
+            ) / 9
+
+            new_img[i, j] = media
+
+    return new_img
+
+def suaviza_cruz(img):
+    new_img = img.copy()
+
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+            media = (
+                img[i][j] +
+                img[i - 1][j] +
+                img[i + 1][j] +
+                img[i][j - 1] +
+                img[i][j + 1]
+            ) / 5
+
+            new_img[i][j] = media
+
+    return new_img
+
+img_suave = img
+bar = Bar("Suavizando...", max=10)
+for i in range(10):
+    img_suave = suaviza_retangulo(img_suave)
+    bar.next()
+bar.finish()
+
+fig = plt.figure()
+
+ax = fig.add_subplot(1, 2, 1)
+ax.imshow(img)
+
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.imshow(img_suave)
+
 plt.show()
