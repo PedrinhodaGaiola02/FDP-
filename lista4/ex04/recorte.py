f7634116b00c9511726cbce89eea940eaa1d48c0
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2024-12-03 13:52:27 -0300

    concreto tratado

---
+++ lista4/ex04/recorte.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import image
+
+img = image.imread('concrete.jpg')
+
+print("Shape:", img.shape)
+
+canalA = img[:, :, 0]
+
+centroX = img.shape[0] // 2
+centroY = img.shape[1] // 2
+raio = 100
+recorte = canalA[(centroX-raio):(centroX+raio), (centroY-raio):(centroY+raio)]
+
+fig = plt.figure()
+
+ax = fig.add_subplot(1, 2, 1)
+ax.set_title("Original")
+ax.imshow(canalA)
+
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.set_title("Recorte")
+ax2.imshow(recorte)
+
+plt.show()
