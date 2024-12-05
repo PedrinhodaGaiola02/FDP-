186e29bc258272033f071830b36bd62b207d9bf2
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2024-12-05 20:32:53 -0300

    iniciando sistema de caching

--- lista4/dynamics/ex06/main.py
+++ lista4/dynamics/ex06/main.py
@@ -1,8 +1,5 @@
-import os
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import image
-from matplotlib import animation
+from animate import animate
 
 dt = 0.1
 tempos = np.arange(0, 20, dt)
@@ -39,6 +36,8 @@ f2[0] = (G*m1*m2)/np.linalg.norm(r1[0]-r2[0])**3 * (r1[0]-r2[0])
 a1[0] = f1[0] / m1
 a2[0] = f2[0] / m2
 
+solution = np.zeros((len(tempos), 9))
+
 for i in range(len(tempos)-1):
     ## calcular novo r
     r1[i+1] = r1[i] + v1[i] * dt + (1/2)*a1[i]*dt**2
@@ -56,30 +55,7 @@ for i in range(len(tempos)-1):
     v1[i+1] = v1[i] + (1/2)*(a1[i] + a1[i+1]) * dt
     v2[i+1] = v2[i] + (1/2)*(a2[i] + a2[i+1]) * dt
 
-# animate
-fig, ax = plt.subplots()
+    solution[i, :] = tempos[i], r1[i, 0], r1[i, 1], v1[i, 0], v1[i, 1], r2[i, 0], r2[i, 1], v2[i, 0], v2[i, 1]
 
-ax.set_title("Simulação de dois corpos")
-ax.set_xlim(-20, 20)
-ax.set_ylim(-20, 20)
-
-img1 = ax.scatter(r1[0, 0], r1[0, 1], s=10*m1)
-img2 = ax.scatter(r2[:, 0], r2[:, 1], s=10*m2)
-
-def update(frameNum, img1, img2, r1, r2):
-    ax.set_title("Simulação de dois corpos - t={:.2f}s".format(tempos[frameNum]))
-    img1.set_offsets(r1[frameNum])
-    img2.set_offsets(r2[frameNum])
-
-animation = animation.FuncAnimation(
-    fig,
-    update,
-    frames=len(tempos),
-    fargs=(img1, img2, r1, r2),
-    cache_frame_data=False,
-    interval=100
-)
-
-plt.show()
-# animation.save("ex06.gif")
-
+animate(r1, r2, m1, m2, tempos)
+np.savetxt("result.txt", solution)
