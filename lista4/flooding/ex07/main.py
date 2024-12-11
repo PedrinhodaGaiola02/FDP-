d967090a07b362ce534b860664acaf71e1ce24a2
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2024-12-11 00:43:10 -0300

    alagado

--- lista4/flooding/ex07/main.py
+++ lista4/flooding/ex07/main.py
@@ -1,7 +1,9 @@
 import numpy as np
+from matplotlib import pyplot as plt, animation
+from copy import deepcopy
 
-N = 10
-p = 0.5
+N = 100
+p = 0.6
 
 if (N % 2 == 0):
     N += 1
@@ -9,13 +11,41 @@ if (N % 2 == 0):
 grid = np.random.choice([0, 2], N*N, p=[p, 1-p]).reshape(N, N)
 grid[N//2][N//2] = 1
 
-def update(grid):
-    def flood(line, column, grid):
-        
-    
-    for line in range(len(grid)):
-        for column in range(len(grid[line])):
-            grid = flood(line, column, grid)
+def update_grid(grid):
+    old_grid = deepcopy(grid)
+
+    for line in range(len(old_grid)):
+        for column in range(len(old_grid[line])):
+            if old_grid[line, column] == 1:
+                if line > 0 and old_grid[line - 1, column] != 2:
+                    grid[line-1, column] = 1
+
+                if line < len(old_grid) - 1 and old_grid[line+1, column] != 2:
+                    grid[line+1, column] = 1
+
+                if column > 0 and old_grid[line, column-1] != 2:
+                    grid[line, column-1] = 1
+
+                if column < len(old_grid[line]) - 1 and old_grid[line, column+1] != 2:
+                    grid[line, column+1] = 1
+
     return grid
 
-print(grid)
+
+def update(frame_num, grid):
+    grid = update_grid(grid)
+
+    plt.title(f"Flooding Algorithm - Frame {frame_num}")
+    return ax.imshow(grid, interpolation='nearest', cmap='Blues')
+
+fig, ax = plt.subplots()
+designer = ax.imshow(grid, interpolation='nearest', cmap='Blues')
+
+ani = animation.FuncAnimation(
+    fig,
+    update,
+    fargs=(grid,),
+    cache_frame_data=False,
+    interval=200,
+)
+plt.show()
